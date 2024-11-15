e54142722e9493f8cfd46d2ed0b2bc6af6baea9d
Vipul Prajapati <vipulprajapati0070@gmail.com>
2024-11-15 08:09:15 +0000

    Target id and class

---
+++ src/main.js
@@ -0,0 +1,18 @@
+const cardNumberText = document.getElementById("card-number");
+const holderNameText = document.getElementById("holder-name-text");
+const expirationDate = document.getElementById("exp-date");
+const cvcText = document.getElementById("cvc-text");
+const cardForm = document.getElementById("card-form");
+const holderNameInput = document.getElementById("holder-name-input");
+const holderError = document.getElementById("holder-error");
+const holderCardNumberInput = document.getElementById("card-number-input");
+const cardNumberError = document.getElementById("number-error");
+const monthInput = document.getElementById("month");
+const yearInput = document.getElementById("year");
+const dateError = document.getElementById("date-error");
+const cvcInput = document.getElementById("cvc");
+const cvcError = document.getElementById("cvc-error");
+const successDialog = document.getElementById("success-dialog");
+const continueBtn = document.getElementById("continue-btn");
+
+
