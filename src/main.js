cdac39f60f076dfdf449096a74b70da5a67e2b2d
Vipul Prajapati <vipulprajapati0070@gmail.com>
2024-11-17 05:07:20 +0000

    some update

--- src/main.js
+++ src/main.js
@@ -16,89 +16,89 @@ const cvcError = document.getElementById("cvc-error");
 const successDialog = document.getElementById("success-dialog");
 const continueBtn = document.getElementById("continue-btn");
 
-
 const nameValidation = () => {
-    let isNameValid = true;
-    let isNumberValid = true;
-    let isDateValid = true;
-    let isCvcValid = true;
-    let isFormValid;
+  let isNameValid = true;
+  let isNumberValid = true;
+  let isDateValid = true;
+  let isCvcValid = true;
+  let isFormValid;
 
-    const holderName = holderNameInput.value;
-    const cvc = cvcInput.value;
-    const holderNumber = holderCardNumberInput.value;
+  const holderName = holderNameInput.value;
+  const cvc = cvcInput.value;
+  const holderNumber = holderCardNumberInput.value;
 
-    // Regular expressions
-    const nameRegex = /[0-9]+/i; // Only letters and spaces
-    const numberRegex = /[0-9]+/i;// Only numbers
+  // Regular expressions
+  const nameRegex = /[0-9]+/i; // Only letters and spaces
+  const numberRegex = /[0-9]+/i; // Only numbers
 
-
-    if (nameRegex.test(holderName)) {
-        holderError.innerText = "Wrong format, text only";
-        isNameValid = false;
+  if (nameRegex.test(holderName)) {
+    holderError.innerText = "Wrong format, text only";
+    isNameValid = false;
 
     // Name validation
-    } if (!holderName) {
-        holderError.innerText = "Can't be blank";
-        isNameValid = false;
-    }
+  }
+  if (!holderName) {
+    holderError.innerText = "Can't be blank";
+    isNameValid = false;
+  }
 
-    if (!numberRegex.test(holderNumber)) {
-        cardNumberError.innerText = "Wrong format, numbers only";
-        isNumberValid = false;
-    }
-    if (!holderNumber) {
-        cardNumberError.innerText = "Can't be blank";
-        isNumberValid = false;
-    }
+  if (!numberRegex.test(holderNumber)) {
+    cardNumberError.innerText = "Wrong format, numbers only";
+    isNumberValid = false;
+  }
+  if (!holderNumber) {
+    cardNumberError.innerText = "Can't be blank";
+    isNumberValid = false;
+  }
 
-    if (!numberRegex.test(month.value) || !numberRegex.test(year.value)) {
-        dateError.innerText = "Wrong format, numbers only";
-    }
+  if (!numberRegex.test(month.value) || !numberRegex.test(year.value)) {
+    dateError.innerText = "Wrong format, numbers only";
+  }
 
-     // Expiry date validation (month and year)
-    if (!month.value || !year.value) {
-        dateError.innerText = "Can't be blank";
-        isDateValid = false;
-    }
+  // Expiry date validation (month and year)
+  if (!month.value || !year.value) {
+    dateError.innerText = "Can't be blank";
+    isDateValid = false;
+  }
 
+  // CVC validation
+  if (!cvc) {
+    cvcError.innerText = "Can't be blank";
+    isCvcValid = false;
+  }
 
-    // CVC validation
-    if (!cvc) {
-        cvcError.innerText = "Can't be blank";
-        isCvcValid = false;
-    }
-
-    if (isCvcValid && isDateValid && isNameValid && isNumberValid) {
-        isFormValid = true;
-    } else {
-        isFormValid = false;
-    }
-    return isFormValid;
-}
+  if (isCvcValid && isDateValid && isNameValid && isNumberValid) {
+    isFormValid = true;
+  } else {
+    isFormValid = false;
+  }
+  return isFormValid;
+};
 
 // form submission event
 cardForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    if (nameValidation()) {
-        holderNameText.innerText = holderNameInput.value;
-        // month aur year ki values le raha hai, aur unhe${month}/${year}format meinexpirationDate` element mein display kar raha hai.
-        expirationDate.innerText = `${monthInput.value}/${yearInput.value}`;
-        // vcInput se CVC ki value le raha hai aur cvcText element mein show kar raha hai.
-        cvcText.innerText = cvcInput.value;
-        const holderCardNumber = holderCardNumberInput.value.replace(" ", "").match(/.{1,4}/g).join(" ");
-        cardNumberText.innerText = holderCardNumber;
-        cardForm.classList.add("hidden");
-        successDialog.classList.remove("hidden");
-    } else {
-        alert("Please fill the form properly.");
-        return;
-    }
-})
-
+  e.preventDefault();
+  if (nameValidation()) {
+    holderNameText.innerText = holderNameInput.value;
+    // month aur year ki values le raha hai, aur unhe${month}/${year}format meinexpirationDate` element mein display kar raha hai.
+    expirationDate.innerText = `${monthInput.value}/${yearInput.value}`;
+    // vcInput se CVC ki value le raha hai aur cvcText element mein show kar raha hai.
+    cvcText.innerText = cvcInput.value;
+    const holderCardNumber = holderCardNumberInput.value
+      .replace(" ", "")
+      .match(/.{1,4}/g)
+      .join(" ");
+    cardNumberText.innerText = holderCardNumber;
+    cardForm.classList.add("hidden");
+    successDialog.classList.remove("hidden");
+  } else {
+    alert("Please fill the form properly.");
+    return;
+  }
+});
 
 // continue button event
 continueBtn.addEventListener("click", () => {
-    successDialog.classList.add("hidden");
-    cardForm.classList.remove("hidden");
-})
+  successDialog.classList.add("hidden");
+  cardForm.classList.remove("hidden");
+});
